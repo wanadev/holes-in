2c42f7494666f6f296a5057b6dc07d410ffa97f7
Léo Nicolle <lnicolle@wanadev.fr>
2017-03-15 16:22:48 +0100

    added uv vert swiag

--- doc/ui.js
+++ doc/ui.js
@@ -1,6 +1,6 @@
 
 let geom1= [{X:0,Y:0},{X:200,Y:0},{X:200,Y:200},{X:0,Y:200}];
-
+/*
 let hole1= [{X:50,Y:50},{X:50,Y:100},{X:100,Y:100},{X:100,Y:50}];
 let hole3= [{X:50,Y:50},{X:50,Y:100},{X:75,Y:100},{X:75,Y:50}];
 
@@ -10,9 +10,19 @@ let hole2= [{X:20,Y:20},{X:20,Y:100},{X:100,Y:100},{X:100,Y:20}];
 let h1 = {path: hole1, depth: 50};
 let h2 = {path: hole2, depth: 50};
 let h3 = {path: hole3, depth: 110};
+*/
+
+let hole1= [{X:40,Y:40},{X:40,Y:90},{X:100,Y:90},{X:100,Y:40}];
+let hole2= [{X:20,Y:20},{X:20,Y:100},{X:100,Y:100},{X:100,Y:20}];
+let hole3= [{X:15,Y:20},{X:80,Y:20},{X:80,Y:50},{X:15,Y:50}];
+
+let h1 = {path: hole1, depth: 150};
+let h2 = {path: hole2, depth: 50};
+let h3 = {path: hole3, depth: 0};
+
 
 let outerShape= {path: geom1, depth: 180};
-let baseholes=[h3,h1];
+let baseholes=[h3,h2,h1];
 
 let holes = JSON.parse(JSON.stringify(baseholes));
 let colors= ["#c02525","#84c025","#8d4ead"];
@@ -27,7 +37,7 @@ let material;
 let texture;
 let options= {inMesh:true, outMesh:false, frontMesh:false, backMesh:false,
             wireframe:false, backFaceCulling:false,normals:false,
-            animate: true
+            animate: true,isoRatioUV:true
             };
 
 let meshDirty=true;
@@ -74,7 +84,8 @@ function updateMesh(){
 
 if(!meshDirty){return;}
 
-  let geom= holesIn.getGeometry(outerShape,holes,options);
+  let geom= holesIn.getGeometry(outerShape,holes,options,options);
+
   let geomMerged= holesIn.mergeMeshes([geom.frontMesh, geom.backMesh, geom.inMesh, geom.outMesh]);
   let nullMesh= false;
   if(!geomMerged){
@@ -173,7 +184,7 @@ function createMesh(scene){
    material.wireframe=options.wireframe;
    material.backFaceCulling = options.backFaceCulling;
 
-   texture= new BABYLON.Texture("../damier.jpg", scene);
+   texture= new BABYLON.Texture("./images/damier.jpg", scene);
    material.diffuseTexture= texture;
    mesh.material= material;
 }
