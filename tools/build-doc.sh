a2af4df6456a5f07ba06232dbd06aaea7d96294f
lnicolle <lnicolle@wanadev.fr>
2018-04-13 17:53:34 +0200

    upidated build doc (copy logo)

--- tools/build-doc.sh
+++ tools/build-doc.sh
@@ -7,4 +7,4 @@ mkdir -p doc.generated/debug
 browserify -t [babelify --presets [ es2015 ] ] -s debuglib debug/lib/index.js -o doc.generated/debug/debuglib.js
 cp debug/index.html doc.generated/debug
 cp debug/style.css doc.generated/debug
-cp -r debug/assets doc.generated/debug
+cp -r debug/assets/ doc.generated/debug/
